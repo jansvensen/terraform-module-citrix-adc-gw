b6eecf56a4614abe8f97c2234fe90b1a6aeb8d40
Sven Jansen <sven.jansen@devicetrust.com>
2023-04-17 20:29:42 +0200

    .

--- main.tf
+++ main.tf
@@ -139,8 +139,7 @@ resource "citrixadc_vpnvserver_vpnsessionpolicy_binding" "gw_vserver_vpnsessionp
 }
 
 resource "citrixadc_authenticationldapaction" "gw_authenticationldapaction" {
-  count              = length(var.adc-gw-authenticationldapaction.name)
-
+  count              = length(var.adc-gw-authenticationldapaction.type)
   
   name               = "auth_act_${element(var.adc-gw-authenticationldapaction["type"],count.index)}_${var.adc-gw.fqdn_int}"
   servername         = element(var.adc-gw-authenticationldapaction["servername"],count.index)
